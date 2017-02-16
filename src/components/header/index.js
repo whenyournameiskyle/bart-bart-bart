db33fbc4e958ac6c60112f2e047713bd01485593
Kyle Anne <kyle.laymon@gmail.com>
2017-02-15 22:26:44 -0800

    Added current time to Header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,13 +7,22 @@ class Header extends React.Component {
   }
 
   render() {
+    let time = new Date()
+    let hour = time.getHours() 
+    hour = (hour > 12) ? (hour - 12) : (hour === 0 ? (hour + 12) : hour) 
+    let minute = time.getMinutes().toString()
+    minute = minute.length === 1 ? ('0' + minute) : minute
+    time = hour + ':' + minute
     return (
-      <div className='header'>
+      <div className='headerContainer'>
         {this.props.showBackButton 
           ? <button className='backButton' onClick={this.props.onClick}>{'←'}</button>
           : null
         }
-        <div className='headerText'>{this.props.text}</div>
+        <div className='headerTextContainer'>
+          <div className='headerTime'>{time}</div>
+          <div className='headerText'>{this.props.text}</div>
+        </div>
       </div>
     )
   }
