cb63162b424642b87685dd0fdbfdc7655fa2e3a7
klaymon <kyle.laymon@gmail.com>
2020-03-09 23:19:26 -0700

    oops I accidentally removed google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-import React  from 'react'
+import React, { useEffect } from 'react'
+import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
 import StationList from '../components/station-list'
 
-const Index = ({ stationList }) => <StationList stationList={stationList} />
+const Index = ({ stationList }) => {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || []
+    function gtag () {
+      window.dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+    gtag('config', 'UA-146149246-1', {
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+      page_title: window.document.title
+    })
+  }, [])
+
+  return (
+    <div>
+      <Head>
+        <title>BART Times</title>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-146149246-1'></script>
+      </Head>
+      <StationList stationList={stationList} />
+    </div>
+  )
+}
 
 Index.getInitialProps = async ctx => {
   const response = await fetch('https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y')
