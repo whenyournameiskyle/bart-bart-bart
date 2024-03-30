2582ed69a13ad9b9bc2a93956958f5a0669f3af1
Kyle Anne <kyle.laymon@gmail.com>
2024-03-29 21:48:07 -0400

    favicon, tipping qr codes

--- app/tip/page.js
+++ app/tip/page.js
@@ -1,15 +1,20 @@
+import Image from 'next/image'
 import { Header } from '../components/header'
 
 export default async function Page() {
   return (
     <div className="container">
       <Header shouldShowBack={true}>Thank you for tipping!</Header>
-      <a href="https://cash.app/$KyleAnne" target="_blank" className="ctaLinkLarge">
-        CashApp: $KyleAnne
-      </a>
-      <a href="https://venmo.com/u/explodedsoda" target="_blank" className="ctaLinkLarge">
-        Venmo: @explodedsoda
-      </a>
+      <div className="qrContainer">
+        <a href="https://cash.app/$KyleAnne" target="_blank" className="ctaLinkLarge">
+          <Image src="/explodedsoda-cashapp-transparent-QR.png" height={500} width={500} alt="QR code for CashApp" layout="responsive" />
+          <div>$KyleAnne</div>
+        </a>
+        <a href="https://venmo.com/u/explodedsoda" target="_blank" className="ctaLinkLarge">
+          <Image src="/explodedsoda-venmo-transparent-QR.png" height={500} width={500} alt="QR code for Venmo" layout="responsive" />
+          <div>@explodedsoda</div>
+        </a>
+      </div>
     </div>
   )
 }
