659c45d8b8530cef070a627d784578664bc7c767
Kyle Anne <kyle.laymon@gmail.com>
2024-05-17 17:05:58 -0400

    metadata and package updates

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,15 @@
+import { Suspense } from 'react'
 import { Analytics } from '@vercel/analytics/react'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
-import { Suspense } from 'react'
 
 export const metadata = {
   title: 'BART Times',
   description: 'Simple site to get Bay Area Rapid Transit (BART) train times',
-  keywords: 'Bay Area Rapid Transit, BART, Bay Area Rapid Transit times, BART train times, san francisco train times, berkeley train times, oakland train times, BART webapp, BART browswer app',
+  keywords:
+    'Bay Area Rapid Transit, BART, Bay Area Rapid Transit times, BART train times, San Francisco train times, Berkeley train times, Oakland train times, BART webapp, BART browser app, BART schedule, BART stations, BART route, BART updates, BART planner, BART travel planner, BART trip planner',
   language: 'English',
+  metadataBase: process.env.VERCEL_URL ? new URL(`https://bartti.me`) : new URL(`http://localhost:${process.env.PORT || 3000}`),
 }
 
 const isProduction = process?.env?.NODE_ENV === 'production'
